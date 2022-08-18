4de9d097dff8a89cbad2e5d5eab8732d0c3e7855
akbarob <akbarbadmus07@gmail.com>
2022-08-18 15:15:27 +0100

    express router

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http')
 const morgan = require('morgan')
 const bodyParser =require('body-parser')
 
+
+const dishRouter = require('./routes/dishRouter')
 const hostname ='localhost'
 const port = 3010
 
@@ -11,42 +13,11 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
-app.all('/dishes',(req,res,next) =>{
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    next()
-})
-
-app.get('/dishes',(req,res,next)=>{
-    res.end('will send all the dishes to you!')
-})
-app.post('/dishes',(req,res,next) =>{
-    res.end('Will add the dish :'+ req.body.name+ 'with details'+ req.body.description)
-})
-app.put('/dishes',(req,res,next) =>{
-    res.statusCode= 403
-    res.end('PUT not supported on /dishes')
-})
-app.delete('/dishes',(req,res,next) =>{
-    res.end('Deleting all dishes')
-})
-
-app.get('/dishes/:dishId',(req,res,next)=>{
-    res.end('will send details of the dish:' + req.params.dishId +'to you!')
-})
-app.post('/dishes/:dishId',(req,res,next) =>{
-    res.statusCode= 403
-    res.end('PUT not supported on /dishes/'+ req.params.dishId)})
-
-app.put('/dishes/:dishId',(req,res,next) =>{
-    res.write('Updatind the dish:'+ req.params.dishId)
-    res.end('will update the dish:' + req.body.name + 'with detail:' + req.body.description)
-})
-app.delete('/dishes/:dishId',(req,res,next) =>{
-    res.end('Deleting dishes' + req.params.dishId)
-})
 
 
+
+
+app.use('/dishes', dishRouter)
 app.use(express.static(__dirname + '/public'));
 
 app.use((req,res,next) => {
